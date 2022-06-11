b4fdb1350a20045c68e4467431d7e98e0dcb4f3e
mohamedmagdy007 <mohamed.mo9751@gmail.com>
2022-06-11 10:51:24 +0200

    fatch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Forecost from "./components/Forecost";
 import Inputs from "./components/Inputs";
 import getFormattedWeatherData from "./components/services/weatherService";
@@ -9,38 +9,40 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
   const [query, setQuery] = useState({ q: "alexandria" });
-  const [debounceQuery, setDebounceQuery] = useState(query);
+  const queryUseRef = useRef();
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
-  // useEffect(() => {
-  //   const timeOut = setTimeout(() => {
-  //     setDebounceQuery(query);
-  //   }, 1200);
-  //   return () => {
-  //     clearTimeout(timeOut);
-  //   };
-  // }, [query]);
-
   useEffect(() => {
-    // const message = query.q ? query.q : "current location.";
-    // toast.info("Fetching weather for" + message);
+    queryUseRef.current = query;
+  });
+  const prevQuery = queryUseRef.current;
+  useEffect(() => {
+    const message = query.q ? query.q : "current location.";
+    toast.info("Fetching weather for" + message);
+
     const fetchWeather = async () => {
       await getFormattedWeatherData({ ...query, units }).then((data) => {
-        // toast.success(
-        //   `Successfully fetched weather for ${data.name}, ${data.country}.`
-        // );
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
         setWeather(data);
       });
     };
 
-    // const clearSettimeout = setTimeout(() => {
-    //   if (debounceQuery) {
-    fetchWeather();
-    //   }
-    // }, 1200);
-    // return () => {
-    //   clearTimeout(clearSettimeout);
-    // };
+    if (weather != null) {
+      if (query) {
+        fetchWeather();
+      }
+    } else if (prevQuery !== queryUseRef) {
+      const clearSettimeout = setTimeout(() => {
+        if (query) {
+          fetchWeather();
+        }
+      }, 1500);
+      return () => {
+        clearTimeout(clearSettimeout);
+      };
+    }
   }, [query, units]);
   const foramtBackground = () => {
     if (!weather) return "from-cyan-700 to-blue-700";
